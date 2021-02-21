3df3434850746eb043a6ba03c3e70c6cb3e50539
Ramon Rodrigues <ramon.srodrigues01@gmail.com>
2021-02-21 17:00:57 -0300

    Changed secret key get value from environment

--- vough_backend/vough_backend/settings.py
+++ vough_backend/vough_backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "38sg$ey_)-q^8bs(ns&bc%_@%)&x9iha47pv5-p8l1t19f7&8g"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
